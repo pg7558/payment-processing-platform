3f5721cc811aaf3d9518bba99c27299a102e37a5
prayash <pg@gmail.com>
2026-04-26 15:17:32 +0530

    Initial Setup: project structure and entities

---
+++ src/main/java/com/wallet/payment_system/PaymentSystemApplication.java
@@ -0,0 +1,14 @@
+package com.wallet.payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wallet/payment_system/entity/Transaction.java
@@ -0,0 +1,29 @@
+package com.wallet.payment_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long fromUser;
+    private Long toUser;
+
+    private Double amount;
+
+    private String status;
+
+    private String idempotencyKey;
+
+    private LocalDateTime createdAt;
+}
